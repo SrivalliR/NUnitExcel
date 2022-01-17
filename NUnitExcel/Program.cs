7381cf64bce699b852526112ba95a065abc7cdec
Rahini <Rahini@LAPTOP-V0LLDNHG>
2022-01-17 15:30:44 +0530

    Add project files.

---
+++ NUnitExcel/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Edge;
+
+namespace NUnitExcel
+{
+    class Program
+    {
+        private IWebDriver driver;
+
+        static void Main(string[] args)
+        {
+        }
+        [SetUp]
+        public void Setup()
+        {
+           driver = new ChromeDriver();
+            //driver = new EdgeDriver();
+            driver.Navigate().GoToUrl("https://wwww.google.com");
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(30);
+
+
+        }
+        [TearDown]
+        public void Teardown()
+        {
+           driver.Close();   
+        }
+
+        [Test]
+        public void ExecuteScript()
+        {                   
+            driver.FindElement(By.Id("q")).SendKeys("Specflow");
+
+        }
+
+    }
+}
